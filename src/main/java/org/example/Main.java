efe42f785c9844c73bf917f11e0ca98737dbcf97
Ignacioooooo <107085341+Ignacioooooo@users.noreply.github.com>
2023-04-18 20:20:37 -0300

    En este commit se termino de subir todo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,23 @@
-package org.example;public class Main {
+package org.example;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+ Scanner scanner = new Scanner(System.in);
+ System.out.println("A continuación ingrese nombre:");
+ String nombre = scanner.nextLine();
+        System.out.println("A continuación ingrese apellido");
+        String apellido = scanner.nextLine();
+        System.out.println("A continuación ingrese fecha de nacimiento");
+        String fecha = scanner.nextLine();
+        scanner.close();
+Persona persona = new Persona();
+persona.setNombre(nombre);
+persona.setApellido(apellido);
+persona.setFechadenacimiento(fecha);
+persona.agregarPersona(persona);
+
+System.out.println("Has agregado a:" + persona.getPersonas().toString());
     }
 }
--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -1,2 +1,34 @@
-package org.example;public class Persona {
+package org.example;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Persona {
+    @Getter @Setter
+    private String nombre;
+    @Getter @Setter
+    private String apellido;
+    @Getter @Setter
+    private String fechadenacimiento;
+
+
+    @Getter @Setter
+    private List<Persona> personas;
+    public Persona(){
+        this.personas = new ArrayList<>();
+    }
+    public void agregarPersona(Persona p){
+        this.personas.add(p);
+    }
+    public void quitarPersona(Persona p){
+        this.personas.remove(p);
+    }
+ public String toString(){
+        return getNombre() + getApellido() +getFechadenacimiento();
+ }
 }
